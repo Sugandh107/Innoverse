89d8eb2d9d36044a4841373fcfe74b380e2c3c46
VishnuSangammanavar <vishnusangammanavar25@gmail.com>
2025-10-29 22:00:23 +0530

    Updated footer logo and text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
             <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
                 <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    <img src="/logo.png" class="h-8 me-3" alt="KLS Gogte Institute of Technology" />
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">KLS Gogte Institute of Technology</span>
                 </a>
             </div>
             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
